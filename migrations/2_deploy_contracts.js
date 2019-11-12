5ecb61964093e3241da67a55c16e90833e3a82ad
yangzhao <yangzhao@mcarlo.com>
2019-11-12 18:31:16 +0800

    add some tools

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,24 @@
 const Proxy = artifacts.require("Proxy.sol");
 const MaiProtocol = artifacts.require("MaiProtocol.sol");
 
-module.exports = function (deployer, network, accounts) {
-    /*
+module.exports = async function (deployer, network, accounts) {
     // proxy
     await deployer.deploy(Proxy);
     const proxyInstance = await Proxy.deployed();
-
     // MaiProtocol
     await deployer.deploy(MaiProtocol, Proxy.address);
-
     // add white list
     await proxyInstance.addAddress(MaiProtocol.address);
 
-    console.log(MaiProtocol.address, "added to whitelist of Proxy");
-    */
+    console.log('2_Summary')
+    console.log('=========')
+    console.log('   > Contract Successfully deployed on', network, 'with', accounts[0])
+    console.log('   ------------------------------------------------------------')
+    console.log('   > MaiProtocol:  ', MaiProtocol.address)
+    console.log('   > Proxy      :  ', Proxy.address)
+    console.log('   -------------------------------------------------------------')
 
+    /*
     return deployer.deploy(Proxy).then((proxyInstance) => {
         return deployer.deploy(MaiProtocol, Proxy.address).then(() => {
             return proxyInstance.addAddress(MaiProtocol.address).then(() => {
@@ -23,4 +26,5 @@ module.exports = function (deployer, network, accounts) {
             });
         });
     });
+    */
 };
