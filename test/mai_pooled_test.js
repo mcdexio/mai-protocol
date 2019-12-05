2a3d90797c1fd99a66a82a2952843934e78362b6
zhutianchi <zhutianchi@mcarlo.com>
2019-12-05 11:06:14 +0800

    mai should never have assets

--- test/mai_pooled_test.js
+++ test/mai_pooled_test.js
@@ -160,6 +160,11 @@ contract('MaiPooled', async accounts => {
                 }
             }
         }
+        
+        // mai should never have assets
+        assert.equal(await call(tokens.collateral.methods.balanceOf(exchange._address)), '0', 'mai should never have ctk');
+        assert.equal(await call(tokens.long.methods.balanceOf(exchange._address)), '0', 'mai should never have long');
+        assert.equal(await call(tokens.short.methods.balanceOf(exchange._address)), '0', 'mai should never have short');
     }
 
     it('buy(long) + buy(short) = mint', async () => {
