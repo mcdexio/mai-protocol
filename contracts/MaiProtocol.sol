2e18ffb1d2617ef062054e69e941b6fc1783d294
zhutianchi <zhutianchi@mcarlo.com>
2019-12-05 11:18:26 +0800

    allow trader cancels the order

--- contracts/MaiProtocol.sol
+++ contracts/MaiProtocol.sol
@@ -618,9 +618,8 @@ contract MaiProtocol is LibMath, LibOrder, LibRelayer, LibExchangeErrors, LibOwn
      * @param order The order to be cancelled.
      */
     function cancelOrder(Order memory order) public {
-        // require(msg.sender == order.trader || msg.sender == order.relayer, INVALID_TRADER);
-        require(msg.sender == order.relayer, INVALID_TRADER);
-
+        require(msg.sender == order.trader || msg.sender == order.relayer, INVALID_TRADER);
+        
         bytes32 orderHash = getOrderHash(order);
         cancelled[orderHash] = true;
 
