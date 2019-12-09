4769836ebd656f11590121df4b7ff5bc13c5453c
zhutianchi <zhutianchi@mcarlo.com>
2019-12-09 11:17:25 +0800

    typo

--- migrations/3_minting_pool.js
+++ migrations/3_minting_pool.js
@@ -3,6 +3,6 @@ const MintingPool = artifacts.require('./MintingPool.sol');
 module.exports = async function (deployer, network, accounts) {
     await deployer.deploy(MintingPool);
     console.log('  「 deploy minting pool 」--------------------------------------')
-    console.log('   > MinintPool:  ', MintingPool.address)
+    console.log('   > MintingPool:  ', MintingPool.address)
     console.log('   -------------------------------------------------------------')
 }
